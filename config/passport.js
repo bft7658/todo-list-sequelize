81bf8fa58180e6f3462927d97507c17083e3c3ee
bft7658 <bft7658@gmail.com>
2022-06-23 15:15:20 +0800

    feat: add message partials

--- config/passport.js
+++ config/passport.js
@@ -10,18 +10,21 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          req.flash('warning_msg', 'Email 還未被註冊！')
+          return done(null, false)
         }
-        return bcrypt.compare(password, user.password).then(isMatch => {
-          if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
-          }
-          return done(null, user)
-        })
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              req.flash('warning_msg', '密碼輸入錯誤')
+              return done(null, false)
+            }
+            return done(null, user)
+          })
       })
       .catch(err => done(err, false))
   }))
